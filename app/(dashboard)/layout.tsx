c2e28b84a1ca6bfd878080dae69627c86ae16abc
msm <myatsumon@ucsy.edu.mm>
2023-08-10 10:28:17 +0630

    dashboard home page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }) => {
   return (
     <html lang="en">
       <head></head>
-      <body className="h-screen w-screen rainbow-mesh p-6">
+      <body className="h-screen w-screen candy-mesh p-6">
         <GlassPane className="w-full h-full flex items-center justify-center">
           {children}
         </GlassPane>
