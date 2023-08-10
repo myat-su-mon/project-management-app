fb3705e63bc39c8dcf63ab1894abddfeaffe8786
msm <myatsumon@ucsy.edu.mm>
2023-08-10 21:47:01 +0630

    modal clietn component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Suspense } from "react";
 import ProjectCard from "../../../components/ProjectCard";
 import TaskCard from "../../../components/TaskCard";
+import NewProject from "../../../components/NewProject";
 
 const getData = async () => {
   await delay(2000);
@@ -44,7 +45,9 @@ const Home = async () => {
               </Link>
             </div>
           ))}
-          <div className="w-1/3 p-3">{/* new project here */}</div>
+          <div className="w-1/3 p-3">
+            <NewProject />
+          </div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
           <div className="w-full">
